9ac076a464159c336e3747df9e65d7f44cdb6439
jeidiidx573 <mizobuchi.works@gmail.com>
2019-06-16 00:50:40 +0900

    First commit

---
+++ src/main.js
@@ -0,0 +1,18 @@
+import Vue from 'vue'
+import './plugins/vuetify'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import firebase from 'firebase'
+
+Vue.config.productionTip = false
+const firebaseConfig = require('./mysetting.env')
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
